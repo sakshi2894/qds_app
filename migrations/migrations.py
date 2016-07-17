3f170180eec312f74f15fb0856ae629a4092518b
Rajat Venkatesh <rvenkatesh@qubole.com>
2016-07-17 21:46:14 +0530

    new:usr:Add test for generation of first commit
    RevisionMap has been removed and assimilated into Migrations.
    Test does not check the contents of migration file.

--- migrations/migrations.py
+++ migrations/migrations.py
@@ -1,26 +1,60 @@
 import datetime
+import glob
 import inspect
+import io
 import logging
 import os
 import string
 import tempfile
 from mako.template import Template
 from mako import exceptions
-from revision_map import RevisionMap
+from revision import Revision
 
 
 class Migrations(object):
     def __init__(self, path):
+        self.last = None
+        self.map = {}
+
         self.path = path
         self.base_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        self.revision_map = RevisionMap.create(path)
 
-    def generate(self, message):
-        revision = self.revision_map.next()
+        for file_ in glob.glob("%s/*.py" % path):
+            revision = Revision(path, file_)
+            self.insert(revision)
+
+    def insert(self, revision):
+        self.map[revision.revision] = revision
+
+        if self.last is None or revision.revision > self.last.revision:
+            self.last = revision
+
+    def head(self):
+        return self.map[0] if not self.map else None
+
+    def next(self):
+        return self.last.revision + 1 if self.last is not None else 0
+
+    def list(self):
+        revisions = []
+        for rev in range(self.next()):
+            revisions.append(rev)
+
+        return revisions
+
+    def __str__(self):
+        if self.head is None:
+            return "<EMPTY>"
+        rev_str = ""
+
+        for rev in range(self.next()):
+            rev_str += str(self.map[rev])
+            rev_str += "\n"
+        return rev_str
+
+    def _generate(self, message, out_stream):
+        revision = self.next()
         template = Template(filename="%s/script.py.mako" % self.base_path)
-        destination_name = args.message[:15] if len(message) > 15 else message
-        destination_name = string.replace(destination_name, ' ', '_')
-        destination_path = "%s/%s_%s.py" % (self.path, revision, destination_name)
 
         try:
             output = template.render_unicode(up_revision=revision,
@@ -36,9 +70,21 @@ class Migrations(object):
                 "Template rendering failed; see %s for a "
                 "template-oriented traceback." % fname)
         else:
-            with open(destination_path, 'wb') as f:
-                f.write(output)
-        logging.info("Created new migration `%s` " % destination_path)
+            out_stream.write(output)
 
-    def list(self):
-        return self.revision_map.list()
+    def generate_str(self, message):
+        out_stream = io.StringIO()
+        self._generate(message, out_stream)
+        return out_stream.getvalue()
+
+    def generate(self, message):
+        revision = self.next()
+
+        destination_name = message[:15] if len(message) > 15 else message
+        destination_name = string.replace(destination_name, ' ', '_')
+        destination_path = "%s/%s_%s.py" % (self.path, revision, destination_name)
+
+        out_stream = io.FileIO(destination_path, "wb")
+        self._generate(message, out_stream)
+        logging.info("Created new migration `%s` " % destination_path)
+        return destination_path
--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,7 +1,16 @@
 import pytest
+import os.path
 from migrations import migrations
 
+
 def test_empty_list(tmpdir_factory):
-    dir = tmpdir_factory.mktemp("empty_migrations")
-    mig = migrations.Migrations(dir)
+    path = tmpdir_factory.mktemp("empty_migrations")
+    mig = migrations.Migrations(path)
     assert(len(mig.list()) == 0)
+
+
+def test_generate_first_migration(tmpdir_factory):
+    path = tmpdir_factory.mktemp("first_migration")
+    mig = migrations.Migrations(path)
+    migration_path = mig.generate("First Migration")
+    assert(os.path.exists(migration_path))
