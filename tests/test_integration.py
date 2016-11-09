f8fdbc576c050686b1c7831e9e94e3cfa5ffec0d
Rajat Venkatesh <rvenkatesh@qubole.com>
2016-11-09 21:52:47 +0530

    fix:dev:Fix import statement in test_integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,11 +1,10 @@
-import pytest
 import os.path
-from migrations import migrations
+from qds_app.migrations import Migrations
 
 
 def test_generate_run(tmpdir_factory):
     path = tmpdir_factory.mktemp("first_migration")
-    mig = migrations.Migrations(str(path))
+    mig = Migrations(str(path))
     migration_path = mig.generate("First Migration")
     assert(os.path.exists(migration_path))
     mig.upgrade()
